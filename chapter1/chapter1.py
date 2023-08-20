25e591956af629ca5b5b0890fc7867d6170d4c21
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-20 17:44:07 +0200

    first commit

---
+++ chapter1/chapter1.py
@@ -0,0 +1,29 @@
+# Example, do not modify!
+print(5 / 8)
+
+
+# Print the sum of 7 and 10
+print(7+10)
+
+
+
+# Division
+print(5 / 8)
+
+#Addition
+print(7 + 10)
+
+#Python as a calculator
+# Addition
+print(4+5)
+
+# Subtraction
+print(5-5)
+
+# Multiplication
+print(3*3)
+
+# Division
+print(10/2)
+
+#Variable Assignment
